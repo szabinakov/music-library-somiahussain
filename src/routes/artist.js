4eab4e3dd584b4cef3231c7bc3bc223b366690c2
somia <somiahussain49@gmail.com>
2023-03-05 17:30:18 +0000

    update and patch

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/', artistController.createArtist);
 router.get('/', artistController.getAllArtists);
 router.get('/:id', artistController.getArtistById);
+router.put('/:id', artistController.putArtist);
+router.patch('/:id', artistController.putArtist);
 
 module.exports = router;
